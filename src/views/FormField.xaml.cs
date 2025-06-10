6ba81516c395f1bc504faf98577e563974250e73
Joshua James <jjv.dev@pm.me>
2025-06-10 11:22:47 +0200

    add FormField

---
+++ src/views/FormField.xaml.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All_Inclusive.views;
+
+public partial class FormField : ContentView
+{
+    public static readonly BindableProperty TitleProperty = BindableProperty.Create("Title", typeof(string), typeof(FormField), null);
+    public static readonly BindableProperty PlaceholderProperty = BindableProperty.Create("Placeholder", typeof(string), typeof(FormField), null);
+    public static readonly BindableProperty DescriptionProperty = BindableProperty.Create("Description", typeof(string), typeof(FormField), null);
+    public static readonly BindableProperty KeyboardProperty = BindableProperty.Create("Keyboard", typeof(bool), typeof(FormField), Keyboard.Default);
+    
+    public String Title
+    {
+        get { return (String)GetValue(TitleProperty); }
+        set { SetValue(TitleProperty, value); }
+    }
+
+    public String Placeholder
+    {
+        get { return (String)GetValue(PlaceholderProperty); }
+        set { SetValue(PlaceholderProperty, value); }
+    }
+
+    public String Description
+    {
+        get { return (String)GetValue(DescriptionProperty); }
+        set { SetValue(DescriptionProperty, value); }
+    }
+
+    public Keyboard Keyboard
+    {
+        get { return (Keyboard)GetValue(KeyboardProperty); }
+        set { SetValue(KeyboardProperty, value); }
+    }
+    
+    public FormField()
+    {
+        InitializeComponent();
+        
+        BindingContext = this;
+    }
+}
