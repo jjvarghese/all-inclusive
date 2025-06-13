8dc303d99e2eb8aea226a798226015edc11d4e13
Joshua James <jjv.dev@pm.me>
2025-06-13 10:46:48 +0200

    add native android collectionview fix

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,12 @@
-﻿namespace All_Inclusive;
+﻿#if ANDROID
+using Android.Views;
+using Android.Views.Accessibility;
+using Android.Widget;
+#endif
+
+using Microsoft.Maui.Handlers;
+
+namespace All_Inclusive;
 
 public static class MauiProgram
 {
@@ -13,6 +21,33 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
 
+        LabelHandler.Mapper.AppendToMapping("SuppressTalkBackHint", (handler, view) =>
+        {
+#if ANDROID
+    var nativeLabel = handler.PlatformView;
+    nativeLabel.Clickable = true;
+
+    nativeLabel.SetAccessibilityDelegate(new SuppressActionHintDelegate());
+#endif
+        });
+
         return builder.Build();
     }
 }
+
+#if ANDROID
+
+class SuppressActionHintDelegate : Android.Views.View.AccessibilityDelegate
+{
+    public override void OnInitializeAccessibilityNodeInfo(Android.Views.View host, AccessibilityNodeInfo info)
+    {
+        base.OnInitializeAccessibilityNodeInfo(host, info);
+
+        // Clear action list
+        info.ActionList?.Clear();
+
+        // Optional: prevent it from being seen as a Button
+        info.ClassName = Java.Lang.Class.FromType(typeof(TextView)).CanonicalName;
+    }
+}
+#endif
